44318a61553bdaaf2bceceda67cc608fffa84f0e
xiaokai <kexiaokai@gmail.com>
2019-01-26 17:39:34 +0800

    Initial Commit

---
+++ QRCatcher/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QRCatcher
+//
+//  Created by xiaokai on 1/26/19.
+//  Copyright © 2019 xiaokaike. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ QRCatcher/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  QRCatcher
+//
+//  Created by xiaokai on 1/26/19.
+//  Copyright © 2019 xiaokaike. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
