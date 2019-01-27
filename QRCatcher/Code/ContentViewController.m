e8c191a5c9fed69243fbe736dc81f467e3290db8
xiaokai <kexiaokai@gmail.com>
2019-01-27 21:33:37 +0800

    feat: change to NSTextView

--- QRCatcher/Code/ContentViewController.m
+++ QRCatcher/Code/ContentViewController.m
@@ -10,7 +10,7 @@
 #import "Utils.h"
 
 @interface ContentViewController ()
-@property (strong) IBOutlet NSTextField *qrResultTextField;
+@property (strong) IBOutlet NSTextView *qrStringTextView;
 
 @end
 
@@ -19,20 +19,65 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do view setup here.
+    [self.qrStringTextView setDrawsBackground:NO];
+    [self.qrStringTextView setFont:[NSFont labelFontOfSize:16]];
+    [self.qrStringTextView setTextColor:NSColor.darkGrayColor];
 }
 
 
 - (void)viewDidAppear {
     [super viewDidAppear];
+
+    [self readQRStringFromPasteBoard];
     
     NSString *scanResult = ScanQRCodeOnScreen();
     NSLog(@"scanResult %@", scanResult);
     
     if (scanResult) {
-        self.qrResultTextField.stringValue = scanResult;
+        [self.qrStringTextView setString:scanResult];
     } else {
-        self.qrResultTextField.stringValue = @"empty";
+        [self.qrStringTextView setString:@"empty"];
     }
+}
+
+- (NSString *)readQRStringFromPasteBoard {
+    NSPasteboard *myPasteboard = [NSPasteboard generalPasteboard];
+    NSString *myString = [myPasteboard stringForType:NSPasteboardTypeString];
+    NSData *pasteboardData = [myPasteboard dataForType:NSPasteboardTypePNG];
+    NSLog(@"%@, and data %@", myString, pasteboardData);
+    
+    NSPasteboardItem *dataHolder = [[NSPasteboardItem alloc] init];
+    for (NSString *type in [myPasteboard types]) {
+        //Get each type's data and add it to the new dataholder
+        NSLog(@"data type %@", type);
+        NSData *data = [[myPasteboard dataForType:type] mutableCopy];
+        if (data) {
+            [dataHolder setData:data forType:type];
+        }
+    }
+    
+    return nil;
+}
+
+- (NSArray *)readFromPasteBoard {
+    NSPasteboard *pasteboard= [NSPasteboard generalPasteboard];
+    NSMutableArray *pasteboardItems = [NSMutableArray array];
+    
+    for (NSPasteboardItem *item in [pasteboard pasteboardItems]) {
+        //Create new data holder
+        NSPasteboardItem *dataHolder = [[NSPasteboardItem alloc] init];
+        //For each type in the pasteboard's items
+        for (NSString *type in [item types]) {
+            //Get each type's data and add it to the new dataholder
+            NSLog(@"data type %@", type);
+            NSData *data = [[item dataForType:type] mutableCopy];
+            if (data) {
+                [dataHolder setData:data forType:type];
+            }
+        }
+        [pasteboardItems addObject:dataHolder];
+    }
+    return pasteboardItems;
 }
 
 - (IBAction)closeButtonPressed:(id)sender
